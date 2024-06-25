f15dcc0f39a7219259cb6c9a1d49f47c0d48cf23
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-25 17:50:16 -0500

    Test Api

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
+server.use(router)
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
