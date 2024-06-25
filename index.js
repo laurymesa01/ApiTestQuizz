2b92b944a9391be37994f027b286507b22dd097e
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-25 18:07:17 -0500

    Fixing "json-server module not found" error

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 4000
 
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT || 3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
